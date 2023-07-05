a363c33695f57797e0e737ce1db9f5ece55bc8ae
A2L1 <pro.arossignol@gmail.com>
2023-07-05 11:29:07 +0200

    First run container

--- api.py
+++ api.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
+from typing import Union
 import uvicorn
 import httpx
 import logging
 import os
 
 class BodyReq(BaseModel):
-    command: str
-    timeout: float | None = 30
+    container: Union[str, None] = None
+    timeout: Union[float, None] = 30
 
 class CTIApi:
 
@@ -17,28 +18,20 @@ class CTIApi:
             description="Fast API"
         )
 
-        @self.app.get("/isup")
+        @self.app.post("/alive")
         async def isup():
             return {'message': 'FastApi is up'}
 
-        @self.app.post("/command")
+        @self.app.post("/run")
         async def forward_request(request_body: BodyReq):
             try:
-                async with httpx.AsyncClient(timeout=request_body.timeout) as client:
-                    logging.debug(f"Request body: {request_body}")
-                    data ={"command": f"{request_body.command}"}
-                    
-                    os.system(request_body.command)
-                    response.raise_for_status()
-                    logging.debug(f"Response from second API: {response.json()}")
-                    return response.json()
+                os.system(f'docker run {request_body.container}')
             except httpx.HTTPError as e:
-                logging.error(f"HTTP Error: {e}")
                 raise HTTPException(status_code=500, detail=f"Error: {e}") from e
 
     
     def run(self):
-        uvicorn.run(self.app, host='0.0.0.0', port=9000)
+        uvicorn.run(self.app, host='0.0.0.0', port=9999)
 
 api = CTIApi()
 api.init()
